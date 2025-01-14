962e6d63f95e2a28c4fc07a03b485a7e17340660
mporada259449 <259449@student.pwr.edu.pl>
2025-01-14 19:02:05 +0000

    Working on test and pytest container

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,11 +1,11 @@
 import pytest
 from app import create_app, db
 from app.config import TestConfig
+from app.models import User
 
 @pytest.fixture(scope='module')
 def app():
     app = create_app(TestConfig)
-    app.config['SECRET_KEY'] = 'test_secret_key'  # Ensure the secret key is set
     with app.app_context():
         db.create_all()
         yield app
@@ -15,20 +15,29 @@ def app():
 def client(app):
     return app.test_client()
 
-def test_login_route(client):
+@pytest.fixture
+def db_session(app):
+    with app.app_context():
+        session = db.session
+        yield session
+        session.rollback()
+        session.remove()
+
+@pytest.fixture
+def create_user(db_session):
+    user = User(
+        id=1,
+        username='admin',
+        password='testpassword',
+        is_admin=True
+    )
+    db_session.add(user)
+    yield db_session.query(User).filter_by(id=1).one()
+    db_session.query(User).filter_by(id=1).delete()
+    db_session.commit()
+
+def test_login_route(client, create_user):
     """Test the login route."""
-    response = client.post('/login', data={'username': 'testuser', 'password': 'testpassword'}, follow_redirects=True)
+    response = client.post('/login', data={'username': 'admin', 'password': 'testpassword'}, follow_redirects=True)
     assert response.status_code == 200
-    assert b'Login' in response.data  # Check for a specific element or text in the HTML content
-
-def test_register_route(client):
-    """Test the register route."""
-    response = client.get('/register')
-    assert response.status_code == 200
-    assert b'Register' in response.data
-
-def test_register_user(client):
-    """Test registering a new user."""
-    response = client.post('/register', data={'username': 'newuser', 'password': 'newpassword'}, follow_redirects=True)
-    assert response.status_code == 200
-    assert b'User registered successfully' in response.data
+    assert b'Login successful' in response.data
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,41 +1,131 @@
-import os
-import io
-import pytest
-from app import create_app, db
-from app.config import TestConfig
+from unittest.mock import patch, MagicMock
+from io import BytesIO
+from app.views import allowed_file, send_compare_request
+from app.logging import log_event
+import requests
+import json
 
-@pytest.fixture(scope='module')
-def app():
-    app = create_app(TestConfig)
-    app.config['SECRET_KEY'] = 'test_secret_key'  # Ensure the secret key is set
-    app.config['UPLOAD_FOLDER'] = '/mnt/images'
-    with app.app_context():
-        db.create_all()
-        yield app
-        db.drop_all()
+#@pytest.fixture
+#def client():
+#    app = Flask(__name__)
+#
+#    @app.route('/compare_images', methods=['POST'])
+#    def compare_images():
+#        if 'file' not in request.files:
+#            return 'No file was provided', 400
+#        # Add more logic here as needed
+#        return 'File received', 200
+#
+#    app.testing = True
+#    return app.test_client()
+#
+#def test_compare_images_no_files(client):
+#    """Test the /compare_images route when no files are provided."""
+#    with client.application.test_request_context('/compare_images', method='POST'):
+#        with patch('flask.request.files', create=True) as mocked_files:
+#            mocked_files.__contains__.return_value = False
+#            
+#            response = client.post('/compare_images')
+#            assert response.status_code == 400
+#            assert b'No file was provided' in response.data
+#
+#def test_compare_images_with_file(client):
+#    """Test the /compare_images route when a file is provided."""
+#    data = {
+#        'file': (io.BytesIO(b'my file contents'), 'test.jpg')
+#    }
+#    response = client.post('/compare_images', data=data, content_type='multipart/form-data')
+#    assert response.status_code == 200
+#    assert b'File received' in response.data
 
-@pytest.fixture(scope='module')
-def client(app):
-    return app.test_client()
+def test_hello(client):
+    response = client.get("/")
+    assert b'Login' in response.data
 
-def test_home_route(client):
-    """Test the home route."""
-    response = client.get('/')
-    assert response.status_code == 200
-    print(response.data.decode())  # Print the response data to debug
-    assert b'Face-app' in response.data  # Adjust the text to match the actual content
-
-def test_compare_images_route(client):
-    """Test the compare images route."""
-    # Ensure the upload folder exists
-    upload_folder = '/mnt/images'
-    if not os.path.exists(upload_folder):
-        os.makedirs(upload_folder)
-
+@patch('os.path.exists')
+def test_compare_images_wrong_extension(mocked_exists, client):
+    mocked_exists.return_value = True
     data = {
-        'image1': (io.BytesIO(b'my file contents'), 'test1.jpg'),
-        'image2': (io.BytesIO(b'my file contents'), 'test2.jpg')
+        'file1': (BytesIO(b'test image data 1'), 'file1.txt'),
+        'file2': (BytesIO(b'test image data 2'), 'file2.txt')
     }
-    response = client.post('/compare_images', data=data, content_type='multipart/form-data')
+
+    response = client.post('/compare_images', data=data, content_type='multipart/form-data', follow_redirects = True)
+
     assert response.status_code == 200
-    assert b'Images compared successfully' in response.data
+    assert b'Unsupported filetype' in response.data
+
+@patch('app.views.send_compare_request')
+def test_compare_images_same_person(mocked_compare, client):
+    mocked_compare.return_value = {'is_similar': True, 'similarity_score': 0.9}
+    data = {
+        'file1': (BytesIO(b'test image data 1'), 'file1.png'),
+        'file2': (BytesIO(b'test image data 2'), 'file2.png')
+    }
+
+    response = client.post('/compare_images', data=data, content_type='multipart/form-data', follow_redirects = True)
+
+    assert response.status_code == 200
+    assert mocked_compare.call_count == 1
+    assert len(mocked_compare.call_args[1].keys()) == 2
+    assert b'Similarity_score: 0.9. This is the same person' in response.data
+
+@patch('app.views.send_compare_request')
+def test_compare_images_not_same_person(mocked_compare, client):
+    mocked_compare.return_value = {'is_similar': False, 'similarity_score': 0.3}
+    data = {
+        'file1': (BytesIO(b'test image data 1'), 'file1.png'),
+        'file2': (BytesIO(b'test image data 2'), 'file2.png')
+    }
+
+    response = client.post('/compare_images', data=data, content_type='multipart/form-data', follow_redirects = True)
+
+    assert response.status_code == 200
+    assert mocked_compare.call_count == 1
+    assert len(mocked_compare.call_args[1].keys()) == 2
+    assert b'Similarity_score: 0.3. This is not the same person' in response.data
+
+@patch('app.views.send_compare_request')
+def test_compare_images_error(mocked_compare, client):
+    mocked_compare.return_value = {'error': 'some error',
+                                   'details': 'error detail',
+                                   'status_code': 404,
+                                   'correlation_id': 'correlation_id'}
+    data = {
+        'file1': (BytesIO(b'test image data 1'), 'file1.png'),
+        'file2': (BytesIO(b'test image data 2'), 'file2.png')
+    }
+
+    response = client.post('/compare_images', data=data, content_type='multipart/form-data', follow_redirects = True)
+
+    assert response.status_code == 200
+    assert mocked_compare.call_count == 1
+    assert len(mocked_compare.call_args[1].keys()) == 2
+    assert b"Error occurred! Status Code: 404, Error: some error" in response.data
+
+@patch('app.views.send_compare_request')
+def test_compare_images_correlation(mocked_compare, client):
+    mocked_compare.return_value = {'correlation_id': 'correlation_id'}
+    data = {
+        'file1': (BytesIO(b'test image data 1'), 'file1.png'),
+        'file2': (BytesIO(b'test image data 2'), 'file2.png')
+    }
+
+    response = client.post('/compare_images', data=data, content_type='multipart/form-data', follow_redirects = True)
+
+    assert response.status_code == 200
+    assert mocked_compare.call_count == 1
+    assert len(mocked_compare.call_args[1].keys()) == 2
+    assert b"Unexpected response from the comparison service" in response.data
+
+def test_allowed_file():
+    result1 = allowed_file('file.txt')
+    result2 = allowed_file('file.png')
+    assert result1 == False
+    assert result2 == True
+
+@patch('requests.post')
+def test_send_compare_request(mock_post):
+    mocked_response = MagicMock()
+    mocked_data = 'saf'
+    mocked_response.content = "asdf"
